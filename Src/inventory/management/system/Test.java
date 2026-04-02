f97e228375ea2ef2a07f764c5ff4f5682ffa4fd7
Aditya patel <apatel89329@gmail.com>
2026-04-02 22:16:33 +0530

    Project Setup

---
+++ Src/inventory/management/system/Test.java
@@ -0,0 +1,8 @@
+package inventory.management.system;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello, Inventory Management System!");
+        System.out.println("Project Setup Done");
+    }
+}
